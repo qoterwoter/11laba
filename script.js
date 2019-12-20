ac0ca6ab3f830cc664a59868f8a73177040e4a2c
qoterwater <tsalapoval@gmail.com>
2019-12-20 18:50:27 +0300

    Добавление элементов

---
+++ script.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'hello, world',
+        id: 10,
+        status: true,
+        count: 1,
+        tempStatus: true,
+        vshow: false
+    }
+});
